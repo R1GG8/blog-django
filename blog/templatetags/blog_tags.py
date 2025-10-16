9a88ac3857c8a531d287410ad39d07a40824799b
R1GG8 <rish@example.com>
2025-10-16 18:57:09 +0500

    similar posts function added

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Q, Count
 from blog.models import Post
 register = template.Library()
 
@@ -13,3 +14,32 @@ def show_tags(post_id):
     post = Post.objects.get(id=post_id)
     tags = post.tags.all()
     return {'tags': tags}
+
+
+@register.inclusion_tag('blog/similar_posts.html')
+def show_similar_posts(post_id, count=3):
+    try:
+        post = Post.objects.get(id=post_id)
+        
+        # Получаем теги текущего поста
+        post_tags_ids = list(post.tags.values_list('id', flat=True))
+        
+        if not post_tags_ids:
+            return {'similar_posts': []}
+        
+        # Более точный подсчет - считаем только общие теги
+        similar_posts = Post.published.filter(
+            tags__id__in=post_tags_ids
+        ).exclude(
+            id=post.id
+        ).annotate(
+            same_tags=Count('tags', filter=Q(tags__id__in=post_tags_ids))
+        ).order_by(
+            '-same_tags',
+            '-published_at'
+        ).distinct()[:count]
+        
+        return {'similar_posts': similar_posts}
+    
+    except Post.DoesNotExist:
+        return {'similar_posts': []}
